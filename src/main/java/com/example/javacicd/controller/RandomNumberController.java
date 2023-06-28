945c6be6143ab301dc692a26991c38f30c68a19b
vlad <vlad.munteanu@orange.com>
2023-06-28 13:24:39 +0300

    feat: overflow test

---
+++ src/main/java/com/example/javacicd/controller/RandomNumberController.java
@@ -0,0 +1,19 @@
+package com.example.javacicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+@RequestMapping("/number")
+public class RandomNumberController {
+
+    @GetMapping
+    public int getRandomNumber() {
+        int min = 0;
+        int max = 100;
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+}
---
+++ src/test/java/com/example/javacicd/controller/RandomNumberControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.javacicd.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class RandomNumberControllerTest {
+    @Autowired
+    RandomNumberController numberController;
+
+    @Test
+    void randomNumberInBoundsTest() {
+        assertThat(101 > numberController.getRandomNumber()).isTrue();
+    }
+
+}
